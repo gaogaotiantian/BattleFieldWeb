f2fe6ed3b0eeb99ee25df8df7608be3e237aea30
gaogaotiantian <gaogaotiantian@hotmail.com>
2018-05-06 14:51:48 -0700

    Some new stuff

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
+import os
+import json
+import base64
+
 import flask
 from flask import Flask, render_template
+import redis
+
+if os.environ.get('REDISCLOUD_URL'):
+    REDIS_URL = os.environ.get('REDISCLOUD_URL')
+else:
+    REDIS_URL = ""
+    
+pool = redis.BlockingConnectionPool.from_url(REDIS_URL, max_connections=9)
+redisConn = redis.Redis(connection_pool = pool)
 
 app = Flask(__name__, static_url_path='/static')
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+app.config['JSON_SORT_KEYS'] = False
+app.secret_key = base64.urlsafe_b64encode(os.urandom(24))
+
+def getResp(t):
+    resp = flask.jsonify(t[1])
+    resp.status_code = t[0]
+    return resp
+
+@app.route('/getGameInfo')
+def getGameInfo():
+    posStr = redisConn.get('playerPos')
+    if posStr:
+        pos = json.loads(posStr)
+    return getResp((200, pos))
+    
 @app.route('/')
 def index():
     return render_template('index.html')
--- static/battlefield.js
+++ static/battlefield.js
@@ -1,3 +1,23 @@
+server_url = "http://localhost:8000/"
+game = {"x":0, "y":0}
+function updateGameInfo() {
+    $.ajax({
+        url:server_url + "getGameInfo",
+        method: "GET",
+        dataType: "json",
+        contentType: 'application/json;charset=UTF-8',
+        success: function(data) {
+            if (data['x']) {
+                game['x'] = data['x'];
+            }
+            if (data['y']) {
+                game['y'] = data['y'];
+            }
+            setTimeout(updateGameInfo, 500);
+        }
+    })
+}
+gameObjects = {};
 function preload() {
     this.load.image('tiles', '/static/assets/Tilesheet/tilesheet_complete.png');
     this.load.image('player', 'static/assets/PNG/Man Blue/manBlue_gun.png');
@@ -8,8 +28,17 @@ function create() {
     var map = this.make.tilemap({data: data, tileWidth:64, tileHeight:64});
     var tiles = map.addTilesetImage('tiles');
     var layer = map.createStaticLayer(0, tiles, 0, 0);
-    var player = this.add.sprite(100, 100, 'player');
+    var group = this.add.group();
+    group.create(100, 100, 'player');
 
+    gameObjects['player'] = group;
+}
+
+function update() {
+    console.log(game);
+    console.log(gameObjects['player']);
+    Phaser.Actions.ShiftPosition(gameObjects['player'].getChildren(), game['x']*32, game['y']*32);
+    
 }
 $(function() {
     var config = {
@@ -22,9 +51,11 @@ $(function() {
         },
         scene: {
             preload: preload,
-            create: create
+            create: create,
+            update: update
         }
     };
 
     var game = new Phaser.Game(config);
+    updateGameInfo();
 })
