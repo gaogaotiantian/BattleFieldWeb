58758fe5cfed6df1e43d9cf9f53f31b4b15429ec
gaogaotiantian <gaogaotiantian@hotmail.com>
2018-05-08 17:21:11 -0700

    Add some blaster files, update the game

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 import json
 import base64
+import random
 
 import flask
 from flask import Flask, render_template
+from flask_sockets import Sockets
 import redis
+import gevent
 
 if os.environ.get('REDISCLOUD_URL'):
     REDIS_URL = os.environ.get('REDISCLOUD_URL')
@@ -19,17 +22,59 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.config['JSON_SORT_KEYS'] = False
 app.secret_key = base64.urlsafe_b64encode(os.urandom(24))
 
+sockets = Sockets(app)
+
+class GameBackend(object):
+    def __init__(self):
+        self.clients = []
+    
+    def register(self, client):
+        self.clients.append(client)
+
+    def send(self, client, data):
+        try:
+            client.send(data)
+        except Exception:
+            self.clients.remove(client)
+    def getData(self):
+        if random.randint(1, 6) == 1:
+            posStr = redisConn.get('staticMapInfo')
+        else:
+            posStr = redisConn.get('dynamicGameInfo')
+        return posStr.decode('utf-8')
+
+    def run(self):
+        while True:
+            data = self.getData()
+            if data:
+                for client in self.clients:
+                    gevent.spawn(self.send, client, data)
+
+    def start(self):
+        gevent.spawn(self.run)
+
+game = GameBackend()
+game.start()
+
 def getResp(t):
     resp = flask.jsonify(t[1])
     resp.status_code = t[0]
     return resp
 
-@app.route('/getGameInfo')
-def getGameInfo():
-    posStr = redisConn.get('playerPos')
-    if posStr:
-        pos = json.loads(posStr)
-    return getResp((200, pos))
+
+@sockets.route('/getGameInfo')
+def getGameInfo(ws):
+    game.register(ws)
+    while not ws.closed:
+        gevent.sleep(0.02)
+
+@sockets.route('/sendAction')
+def sendAction(ws):
+    while not ws.closed:
+        gevent.sleep(0.02)
+        message = ws.receive()
+        if message:
+            redisConn.rpush('actionQueue', message)
     
 @app.route('/')
 def index():
--- static/battlefield.js
+++ static/battlefield.js
@@ -1,5 +1,49 @@
-server_url = "http://localhost:8000/"
-game = {"x":0, "y":0}
+url = "localhost:8000"
+server_url = "http://"+url
+ws_url = "ws://"+url
+
+game = {};
+gameObjects = {};
+phaser = null;
+
+var gameInfoSocket = new ReconnectingWebSocket(ws_url+"/getGameInfo");
+var sendActionSocket = new ReconnectingWebSocket(ws_url+"/sendAction");
+
+gameInfoSocket.onmessage = function(message) {
+    var data = JSON.parse(message.data);
+    if (data['infoType']) {
+        if (data['infoType'] == 'dynamicGameInfo') {
+            if (data['players']) {
+                game['players'] = data['players'];
+                game['bullets'] = data['bullets'];
+                game['timestamp'] = data['timestamp'];
+                var deltaTime = Date.now() / 1000.0 - game['timestamp'];
+                if (!game['clientDeltaTime'] || Math.abs(deltaTime - game['clientDeltaTime']) > 0.5) {
+                    game['clientDeltaTime'] = deltaTime;
+                }
+            }
+        } else if (data['infoType'] == 'staticMapInfo') {
+            if (data['map']) {
+                game['map'] = data['map'];
+                //updateMap();
+            }
+        }
+    }
+}
+
+function sendMove(x, y) {
+    sendActionSocket.send(JSON.stringify({
+        "actionType": "move",
+        "x": x,
+        "y": y 
+    }));
+}
+
+function sendShoot() {
+    sendActionSocket.send(JSON.stringify({
+        "actionType": "shoot"
+    }));
+}
 function updateGameInfo() {
     $.ajax({
         url:server_url + "getGameInfo",
@@ -7,44 +51,89 @@ function updateGameInfo() {
         dataType: "json",
         contentType: 'application/json;charset=UTF-8',
         success: function(data) {
-            if (data['x']) {
-                game['x'] = data['x'];
-            }
-            if (data['y']) {
-                game['y'] = data['y'];
-            }
+        },
+        complete: function() {
             setTimeout(updateGameInfo, 500);
         }
     })
 }
-gameObjects = {};
+function getObjectPosition(obj) {
+    var deltaTime = Date.now() / 1000.0 - game['clientDeltaTime'] - game['timestamp'];
+    var ret = {};
+    ret['x'] = obj['x'] + Math.cos(obj['angle']) * deltaTime * obj['speed'];
+    ret['y'] = obj['y'] + Math.sin(obj['angle']) * deltaTime * obj['speed'];
+    return ret;
+}
+function updateMap() {
+    var map = gameObjects['map'];
+    var data = game['map']['tile'];
+    map.putTilesAt(data, 0, 0);
+}
 function preload() {
     this.load.image('tiles', '/static/assets/Tilesheet/tilesheet_complete.png');
     this.load.image('player', 'static/assets/PNG/Man Blue/manBlue_gun.png');
+    this.load.image('bullet', '/static/assets/blaster/images/image95.png');
 }
 
 function create() {
-    var data = [[1,2,3],[4,5,6]];
-    var map = this.make.tilemap({data: data, tileWidth:64, tileHeight:64});
+    var map = this.make.tilemap({tileWidth:32, tileHeight:32, width: 30, height: 30});
     var tiles = map.addTilesetImage('tiles');
-    var layer = map.createStaticLayer(0, tiles, 0, 0);
-    var group = this.add.group();
-    group.create(100, 100, 'player');
+    var layer = map.createBlankDynamicLayer('layer', tiles);
 
-    gameObjects['player'] = group;
+    gameObjects['player'] = {};
+    gameObjects['bullets'] = {};
+    gameObjects['map'] = map;
+
+    // events
+    this.input.on('pointerdown', function(pointer) {
+        sendMove(pointer.x, pointer.y);
+    }, this);
+
+    this.input.keyboard.on('keydown_SPACE', function(event) {
+        sendShoot();
+    })
 }
 
 function update() {
-    console.log(game);
-    console.log(gameObjects['player']);
-    Phaser.Actions.ShiftPosition(gameObjects['player'].getChildren(), game['x']*32, game['y']*32);
+    for (var i in game['players']) {
+        var player = game['players'][i];
+        var pos = getObjectPosition(player);
+        var id = player['id'];
+        if (gameObjects['player'][id]) {
+            var gameObj = gameObjects['player'][id]
+            Phaser.Actions.ShiftPosition(gameObj.getChildren(), pos['x'], pos['y']);
+            gameObj.getChildren()[0].rotation = player['angle'];
+        } else {
+            var group = this.add.group();
+            group.create(player['x'], player['y'], 'player');
+            gameObjects['player'][id] = group;
+        }
+    }
+
+    for (var i in game['bullets']) {
+        var bullet = game['bullets'][i];
+        var pos = getObjectPosition(bullet);
+        var id = bullet['id'];
+        if (gameObjects['bullets'][id]) {
+            var gameObj = gameObjects['bullets'][id]
+            Phaser.Actions.ShiftPosition(gameObj.getChildren(), pos['x'], pos['y']);
+            gameObj.getChildren()[0].rotation = bullet['angle'];
+        } else {
+            var group = this.add.group();
+            group.create(bullet['x'], bullet['y'], 'bullet');
+            gameObjects['bullets'][id] = group;
+        }
+    }
+    if (game['map'] && game['map']['tile']) {
+        updateMap(this, game['map']['tile']);
+    }
     
 }
 $(function() {
     var config = {
         type: Phaser.AUTO,
-        width: 800,
-        height: 800,
+        width: 960,
+        height: 960,
         physics: {
             default: 'arcade',
             arcade: {}
@@ -56,6 +145,5 @@ $(function() {
         }
     };
 
-    var game = new Phaser.Game(config);
-    updateGameInfo();
+    phaser = new Phaser.Game(config);
 })
